8f68dbb506e87a879cc3baba27d7546728d3da90
andy922200-m1 <andy922200@gmail.com>
2022-05-17 21:22:32 +0800

    1. exreact routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+/*TODO postRouter*/
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const userRouter = require('./users')
+const generalRouter = require('./general') // includes 404 missing routes
 
-module.exports = router;
+module.exports = (app) => {
+  // app.use('/posts', postRouter),
+  app.use('/users', userRouter),
+  app.use('/', generalRouter)
+}
