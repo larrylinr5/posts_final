aa40d79ceded1d9746085ff4568113c60f46da33
andy lien <andy922200@gmail.com>
2022-05-18 16:54:16 +0800

    1. add jwt-token and isAuth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,18 +2,20 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controller/auth_controller')
 const { appError, handleErrorAsync } = require('../utils/errorHandler')
+const { isAuth, generateJwtToken } = require('../middleware/auth')
 
 router.get('/google', handleErrorAsync(authController.google.auth))
 router.get('/google/callback', handleErrorAsync(authController.google.execCallback), handleErrorAsync (async (req, res, next) => {
     if(req.user){
-        res.status(200)
-            .json({
-                status: 'success',
-                data: {
-                    user: req.user
-                },
-                message: 'Login via google successfully'
-            })
+        const token = await generateJwtToken(req.user.id)
+
+        if(token){
+            res.cookie('x-token', token)
+            res.redirect(`http://localhost:8080/section7/7-2/test.html?token=${token}`)
+        }else{
+            return next(appError('401','','No permission to generate token', next))
+        }
+
     }else{
         res.status(401)
             .json({
