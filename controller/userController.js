f08ef609cb5b023a0166d299dcc9e4ca48a1d4ae
charlie50503 <cate50503@gmail.com>
2022-05-21 19:48:10 +0900

    新增 - sign-in api

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,4 @@
 const { appError, handleErrorAsync } = require('../utils/errorHandler')
-const { getHttpResponse } = require('../utils/successHandler')
 
 const bcrypt = require('bcryptjs')
 const { generateJwtToken } = require('../middleware/auth')
@@ -7,7 +6,6 @@ const { generateJwtToken } = require('../middleware/auth')
 const User = require('../models/userModel')
 const Follow = require('../models/followModel')
 const Validator = require('../utils/validator')
-
 const users = {
   async signUp(req, res, next) {
     const validatorResult = Validator.signUp(req.body)
@@ -24,14 +22,40 @@ const users = {
         password,
       })
     } catch (error) {
-      console.log(error)
-      return res.status(400).json({
-        status: 'false',
-        msg: '已註冊此用戶',
-      })
+      if(error.code===11000){
+        return  next(appError('400', '資料內容', '已註冊此用戶', next))
+      }
+      return  next(appError('400', '資料內容有誤', '不明原因錯誤', next))
     }
 
     const { _id } = newUser
+    const token = await generateJwtToken(_id)
+    if(token.length===0) {
+      return next(appError('400', '資料內容', 'token建立失敗', next))
+    }
+    res.json({
+      status: 'success',
+      token,
+    });
+  },
+  async signIn(req, res, next) {
+    const validatorResult = Validator.signIn(req.body)
+    if (!validatorResult.status) {
+      return next(appError('400', '格式錯誤', validatorResult.msg, next))
+    }
+    // password = await bcrypt.hash(req.body.password, 12)
+    const { email,password } = req.body
+    const user = await User.findOne({
+        email
+      }).select('+password');
+    if(!user) {
+      return next(appError('400', '無此資料', '不存在該筆資料', next));
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if(!auth) {
+      return next(appError('400', '內容錯誤', '您的密碼不正確', next));
+    }
+    const { _id } = user
     const token = await generateJwtToken(_id)
     if(token.length===0) {
       return res.status(400).json({
--- utils/validator.js
+++ utils/validator.js
@@ -7,7 +7,6 @@ module.exports = class Validator {
    * @returns {status,msg}
    */
   static signUp({ nickName, email, password, confirmPassword }) {
-    console.log({ nickName, email, password, confirmPassword })
     if (!nickName || !email || !password || !confirmPassword) {
       return {
         status: false,
@@ -27,6 +26,30 @@ module.exports = class Validator {
       }
     }
 
+    if (!validator.isLength(password, { min: 8 })) {
+      return {
+        status: false,
+        msg: '密碼少於8位數!',
+      }
+    }
+    return {
+      status: true,
+      msg: 'success',
+    }
+  }
+  static signIn({ email, password }) {
+    if (!email || !password) {
+      return {
+        status: false,
+        msg: '帳號密碼不可為空!',
+      }
+    }
+    if (!validator.isEmail(email)) {
+      return {
+        status: false,
+        msg: 'Email 格式不正確!',
+      }
+    }
     if (!validator.isLength(password, { min: 8 })) {
       return {
         status: false,
