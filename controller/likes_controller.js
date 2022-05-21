8abb1fa47ffe307660d440e455cf38617fc98130
Adam Hsu <projectnba77@gmail.com>
2022-05-22 06:36:49 +0800

    feat: 取得個人按讚列表 API

---
+++ controller/likes_controller.js
@@ -0,0 +1,42 @@
+const { handleErrorAsync } = require('../utils/errorHandler');
+const getHttpResponse = require('../utils/successHandler');
+const Post = require('../models/postModel')
+
+const like = {
+    getUserLikeList: handleErrorAsync(async (req, res, next) => {
+
+        const { user, query } = req
+        const keyword = query.keyword ? query.keyword : ''
+        let page = Number(query.page - 1) // 當前頁數
+        let pageNum = query.pageNum ? Number(query.pageNum) : 10 // 一頁顯示幾筆資料
+
+        // 搜尋條件
+        const filter = {
+            "$in": user._id,
+            'likes.0': {$exists: true},
+            $or: [
+                { content: { $regex: keyword } },
+            ],
+        }
+
+        // 倒序: false，升序: true
+        const sort = !query.sort ? -1 : 1
+        // 用戶有按讚的所有貼文
+        const userAllPost = await Post.find(filter).skip(page).limit(pageNum).sort({ 'createdAt': sort })
+
+        let total = userAllPost.length // 總資料筆數
+        let totalPages = Math.ceil(userAllPost.length / pageNum) // 一共顯示幾頁
+
+        const resData = {
+            result: userAllPost.length === 0 ? '您尚未按讚，故無該資料' : userAllPost,
+            page,
+            pageNum,
+            total,
+            totalPages
+        }
+
+        res.status(201).json(getHttpResponse(resData));
+    }),
+}
+
+module.exports = like
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const PostController = require('../controller/postController');
+const LikesControllers = require('../controller/likes_controller')
 const { isAuth } = require('../middleware/auth')
 
 // 張貼個人動態 - 新增貼文
 router.post('/', isAuth, (req, res, next) => {
-  PostController.postOnePost(req, res, next);
+    PostController.postOnePost(req, res, next);
 });
+
+// 取得個人按讚列表
+router.get('/likes', isAuth, (req, res, next) => {
+    LikesControllers.getUserLikeList(req, res, next)
+})
 
 module.exports = router;
