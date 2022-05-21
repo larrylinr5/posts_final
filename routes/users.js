20b862770d0e7e9d041574e9894dd8e06db637ac
charlie50503 <cate50503@gmail.com>
2022-05-21 17:34:52 +0900

    更新 - 將註冊功能移動到 controller ，並完成response回傳token

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const Validator = require('../utils/validator')
-const appError = require('../utils/errorHandler')
-const bcrypt = require('bcryptjs/dist/bcrypt')
-const User = require('../models/userModel')
+const userController = require('../controller/userController')
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource')
 })
 
-router.post('/sign_up', async function (req, res, next) {
-  const validatorResult = Validator.signUp(req.body)
-  if (!validatorResult.status) {
-    return next(appError('400', validatorResult.status, next))
-  }
-  password = await bcrypt.hash(req.body.password, 12)
-  const { nickName, email } = req.body
-  let newUser
-  try {
-    newUser = await User.create({
-      nickName,
-      email,
-      password,
-    })
-  } catch (error) {
-    console.log(error)
-    return res.status(400).json({
-      status: 'false',
-      msg: '已註冊此用戶',
-    })
-  }
-
-  res.json({
-    status: 'success',
-    user: {
-      token: '',
-      nickName: '',
-      id: '',
-    },
-  })
+router.post('/sign_up', (req, res, next) => {
+  userController.signUp(req, res, next)
 })
 
 module.exports = router
