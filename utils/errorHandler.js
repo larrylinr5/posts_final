cc611392ed1d4dc38a4d609fc2e2222fcbb33888
charlie50503 <cate50503@gmail.com>
2022-05-22 14:44:13 +0900

    更新 - 為 errorHandler處理添加共通回傳值 status

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -15,6 +15,12 @@ const appError = (statusCode, errName, errMessage, next) => {
 }
 
 // async func catch
+/**
+ *
+ * @description - 用來擷取系統錯誤
+ * @param {Function} func - controller function 
+ * @return {Next} - 回傳express Next
+ */
 const handleErrorAsync = function (func) {
     return function (req, res, next) {
         func(req, res, next).catch(
@@ -29,26 +35,41 @@ const handleErrorAsync = function (func) {
 const resErrorDev = (err, res) => {
     res.status(err.statusCode)
         .json({
+            status:'false',
             message: err.message,
             error: err,
             stack: err.stack
         })
 }
+/**
+ * @description - 用來切換是系統錯誤還是自定義錯誤的status值
+ * @param {Error} {statusCode} - error response 用的 statusCode
+ * @return {'false'|'error'} - 回傳值 'false' or 'error' 
+ */
+const resErrorStatus = ({statusCode}) => {
+    if(statusCode===500){
+        return 'error'
+    }
+    return 'false'
+}
+
 
 // Prod 環境下，自己設定的 err 錯誤 
 const resErrorProd = (err, res) => {
+    const resErrorData = {
+        status:'',
+        message: '',
+    }
+    resErrorData.status = resErrorStatus(err)
     if (err.isOperational) {
+        resErrorData.message = err.message
         res.status(err.statusCode)
-            .json({
-                message: err.message,
-            })
+            .json(resErrorData)
     } else {
         console.error('出現重大錯誤', err)
+        resErrorData.message = '系統錯誤，請洽系統管理員'
         res.status(err.statusCode)
-            .json({
-                status: 'error',
-                message: '系統錯誤，請洽系統管理員'
-            })
+            .json(resErrorData)
     }
 }
 
