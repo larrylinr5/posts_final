768a93eceb8b73eea98c7d2a6e11942e8da07ac0
Adam Hsu <projectnba77@gmail.com>
2022-05-22 10:03:53 +0800

    feat: 1. 新增及取消一則貼文的讚 2. 刪除 routes like.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const userRouter = require('./users')
 const postRouter = require('./posts')
+const likeRouter = require('./likes')
 const authRouter = require('./auth')
 const generalRouter = require('./general') // includes 404 missing routes
 
 module.exports = (app) => {
   app.use('/users', userRouter),
   app.use('/posts', postRouter),
+  app.use('/likes', likeRouter),
   app.use('/auth', authRouter),
   app.use('/', generalRouter)
 }
