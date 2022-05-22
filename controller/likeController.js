62f30c866357f219a9a5229a456e191df73857d9
Adam Hsu <projectnba77@gmail.com>
2022-05-22 16:29:37 +0800

    fix: 取得個人按讚列表API， 搜尋條件及回傳Response結構調整

--- controller/likeController.js
+++ controller/likeController.js
@@ -3,7 +3,7 @@ const getHttpResponse = require('../utils/successHandler');
 const Post = require('../models/postModel')
 
 const like = {
-    getUserLikeList: handleErrorAsync(async(req, res, next) => {
+    getUserLikeList: handleErrorAsync(async (req, res, next) => {
 
         const { user, query } = req
         const keyword = query.keyword ? query.keyword : ''
@@ -14,6 +14,7 @@ const like = {
         const filter = {
             '$in': user._id,
             'likes.0': { $exists: true },
+            'logicDeleteFlag': false,
             $or: [
                 { content: { $regex: keyword } },
             ],
@@ -21,37 +22,40 @@ const like = {
 
         // 倒序: desc，升序: asc
         const sort = query.sort === 'desc' ? -1 : query.sort === 'asc' ? 1 : 'asc'
-            // 用戶有按讚的所有貼文，隱藏comments欄位
+        // 用戶有按讚的所有貼文，隱藏comments欄位
         const userAllPost = await Post.find(filter, { 'comments': false }).populate({ path: 'editor', select: 'nickName avatar' }).skip(page).limit(pageNum).sort({ 'createdAt': sort })
 
         let total = userAllPost.length // 總資料筆數
         let totalPages = Math.ceil(userAllPost.length / pageNum) // 一共顯示幾頁
 
         const resData = {
-            list: userAllPost.length === 0 ? '您尚未按讚，故無該資料' : userAllPost,
-            page,
-            pageNum,
-            total,
-            totalPages
+            message: userAllPost.length === 0 ? '您尚未按讚，故無該資料' : '取得資料成功',
+            list: userAllPost,
+            page: {
+                totalPages, // 總頁數
+                'currentPage': page, // 當前頁數
+                'perPage': pageNum, // 一頁顯示資料筆數
+                'totalDatas': total // 資料總筆數
+            },
         }
 
         res.status(201).json(getHttpResponse(resData));
     }),
 
-    addPostLike: handleErrorAsync(async(req, res, next) => {
+    addPostLike: handleErrorAsync(async (req, res, next) => {
         const { user, params: { postId } } = req
 
         await Post.findOneAndUpdate({ postId }, { $addToSet: { likes: user._id } })
 
-        res.status(201).json(getHttpResponse({message: '加入按讚成功'}));
+        res.status(201).json(getHttpResponse({ message: '加入按讚成功' }));
     }),
 
-    delPostLike: handleErrorAsync(async(req, res, next) => {
+    delPostLike: handleErrorAsync(async (req, res, next) => {
         const { user, params: { postId } } = req
 
         await Post.findOneAndUpdate({ postId }, { $pull: { likes: user._id } })
 
-        res.status(201).json(getHttpResponse({message: '移除按讚成功'}));
+        res.status(201).json(getHttpResponse({ message: '移除按讚成功' }));
     })
 }
 
