8420c59770b004a6e18fbabab588af0b3649e5f2
Adam Hsu <projectnba77@gmail.com>
2022-05-22 11:26:04 +0800

    fix: 取得個人按讚列表API 1.隱藏 comments 欄位 2.editor 欄位顯示nickName及avatar 3.response 結構改為list

--- controller/likeController.js
+++ controller/likeController.js
@@ -12,7 +12,7 @@ const like = {
 
         // 搜尋條件
         const filter = {
-            "$in": user._id,
+            '$in': user._id,
             'likes.0': { $exists: true },
             $or: [
                 { content: { $regex: keyword } },
@@ -21,14 +21,14 @@ const like = {
 
         // 倒序: false，升序: true
         const sort = !query.sort ? -1 : 1
-        // 用戶有按讚的所有貼文
-        const userAllPost = await Post.find(filter).skip(page).limit(pageNum).sort({ 'createdAt': sort })
+        // 用戶有按讚的所有貼文，隱藏comments欄位
+        const userAllPost = await Post.find(filter, {'comments': false}).populate({ path: 'editor', select: 'nickName avatar'}).skip(page).limit(pageNum).sort({ 'createdAt': sort })
 
         let total = userAllPost.length // 總資料筆數
         let totalPages = Math.ceil(userAllPost.length / pageNum) // 一共顯示幾頁
 
         const resData = {
-            result: userAllPost.length === 0 ? '您尚未按讚，故無該資料' : userAllPost,
+            list: userAllPost.length === 0 ? '您尚未按讚，故無該資料' : userAllPost,
             page,
             pageNum,
             total,
