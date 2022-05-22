c9fdcc7332943a197490c4a46ef468c71dbc0844
charlie50503 <cate50503@gmail.com>
2022-05-22 14:49:30 +0900

    更新 - 為 sign_up 跟 sign_in api 添加 middleware

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controller/userController')
-
+const { handleErrorAsync } = require('../utils/errorHandler');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource')
 })
 
-router.post('/sign_up', (req, res, next) => {
+router.post('/sign_up', handleErrorAsync(async (req, res, next) => {
   userController.signUp(req, res, next)
-})
-router.post('/sign_in', (req, res, next) => {
+}))
+router.post('/sign_in', handleErrorAsync(async (req, res, next) => {
   userController.signIn(req, res, next)
-})
+}))
 
 module.exports = router
