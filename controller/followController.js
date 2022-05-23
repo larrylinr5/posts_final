7e8c8613a804e063e723dd23df232e165e5a037c
Hobby Lee <hobbylee@HobbydeMacBook-Pro.local>
2022-05-23 22:54:22 +0800

    fix: post.js 衝突

--- controller/followController.js
+++ controller/followController.js
@@ -1,5 +1,5 @@
 const { appError, handleErrorAsync } = require("../utils/errorHandler");
-const { getHttpResponse } = require('../utils/successHandler');
+const getHttpResponse = require('../utils/successHandler');
 
 const validator = require('validator');
 
@@ -10,14 +10,14 @@ const follows = {
   postFollow: handleErrorAsync(async (req, res, next) => {
     const { user } = req;
     const otherUser = req.params.id;
-    if(otherUser === user.id) {
+    if (otherUser === user.id) {
       return next(appError(400, '無法追蹤自己', '無法追蹤自己', next));
     }
     const existedFollowing = await Follow.findOne({
       editor: user.id,
       following: otherUser
     })
-    if(existedFollowing) {
+    if (existedFollowing) {
       return next(appError(400, '已追蹤該用戶', '已追蹤該用戶', next));
     }
     await Follow.create({
@@ -30,16 +30,16 @@ const follows = {
     });
   }),
   deleteFollow: handleErrorAsync(async (req, res, next) => {
-    const {user} = req;
+    const { user } = req;
     const otherUser = req.params.id;
-    if(otherUser === user.id) {
+    if (otherUser === user.id) {
       return next(appError(400, '無法取消追蹤自己', '無法取消追蹤自己', next));
     }
     const existedFollowing = await Follow.findOne({
       editor: user.id,
       following: otherUser
     });
-    if(!existedFollowing) {
+    if (!existedFollowing) {
       return next(appError(400, '尚未追蹤該用戶', '尚未追蹤該用戶', next));
     }
     await Follow.deleteOne({
@@ -49,6 +49,20 @@ const follows = {
     res.status(200).json({
       status: 'success',
       message: '取消追蹤成功'
+    });
+  }),
+  // 取得個人所有追蹤列表
+  getFollowList: handleErrorAsync(async (req, res, next) => {
+    // 尋找 follow 裡面 editor 為自己的資料
+    const list = await Follow.find({ editor: req.user.id }).populate({
+      path: 'following',
+      select: 'nickName avatar'
+    })
+
+    // 回傳結果
+    res.json({
+      status: 'success',
+      data: list
     });
   })
 }
