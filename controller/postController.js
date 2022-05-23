6a5a1d4eb7f11d347a0b80628ee5e7ce903f399c
andy922200-m1 <andy922200@gmail.com>
2022-05-23 21:54:04 +0800

    1. finish getAllPosts api

--- controller/postController.js
+++ controller/postController.js
@@ -8,6 +8,49 @@ const Post = require('../models/postModel');
 const Comment = require('../models/commentModel');
 
 const posts = {
+  // 取得全部貼文或個人全部貼文
+  getAllPosts: handleErrorAsync(async (req, res, next) => {
+    const { query, params: { userId } } = req
+    const timeSort = query.sort === "asc" ? 1 : query.sort === 'desc' ? -1 : 'asc'
+    const currentPage = Number(query.page)
+    const perPage = query.perPage ? Number(query.perPage) : 10
+    const queryString = query.q !== undefined
+      ? {
+        $or: [
+          { "content": new RegExp(query.q.trim()) }
+        ],
+        'logicDeleteFlag': false,
+      }
+      : {}
+
+    if(userId){
+      queryString.editor = userId
+    }
+
+    // 向 DB 取得貼文資料
+    const allPosts = await Post.find(queryString, { 'comments': false }).populate({
+      path: 'editor',
+      select: 'nickName avatar'
+    }).skip((currentPage - 1) * perPage).limit(perPage).sort({ 'createdAt': timeSort })
+
+    const total = allPosts.length
+    const totalPages = Math.ceil(allPosts.length / perPage)
+    const resData = {
+      message: allPosts.length === 0 ? '搜尋無資料' : '成功取得搜尋貼文',
+      list: allPosts,
+      page: {
+        totalPages,
+        currentPage,
+        perPage,
+        totalDatas: total,
+        has_pre: allPosts.length === 0 ?  false : currentPage > 1,
+        has_next: allPosts.length === 0 ?  false : currentPage < totalPages
+      }
+    }
+
+    res.status(200).json(getHttpResponse(resData));
+  }),
+
   // 新增貼文
   postOnePost: handleErrorAsync(async (req, res, next) => {
     const { user, body: { content, image } } = req;
@@ -30,7 +73,7 @@ const posts = {
     const newPost = await Post.find({}).sort({_id:-1}).limit(1).select('-logicDeleteFlag');
 
     res.status(201).json(getHttpResponse(newPost));
-  }),
+  })
 }
 
 module.exports = posts;
