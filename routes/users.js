fa45125e892f68200819c18c5084aa76256aa832
Hobby Lee <hobbylee@HobbydeMacBook-Pro.local>
2022-05-23 22:55:37 +0800

    fix: post.js, users.js 衝突

--- routes/users.js
+++ routes/users.js
@@ -29,5 +29,9 @@ router.patch('/updatePassword', isAuth, handleErrorAsync(async (req, res, next) 
 router.post('/follows/:id', isAuth, checkUserId, followController.postFollow);
 
 router.delete('/follows/:id', isAuth, checkUserId, followController.deleteFollow);
+/* 取得個人所有追蹤列表 */
+router.get('/follows', isAuth, (req, res, next) => {
+  followController.getFollowList(req, res, next)
+})
 
 module.exports = router
