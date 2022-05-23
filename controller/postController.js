0eecd4ff6cbe5f9dcded990f992fc89c62d77cf8
hiYifang <a6426320@gmail.com>
2022-05-24 00:14:15 +0800

    feat: 修改貼文 API

--- controller/postController.js
+++ controller/postController.js
@@ -31,6 +31,35 @@ const posts = {
 
     res.status(201).json(getHttpResponse(newPost));
   }),
+  // 修改貼文
+  patchOnePost: handleErrorAsync(async (req, res, next) => {
+    const { user, body: { content, image }, params: { postId } } = req;
+
+    // 判斷圖片開頭是否為 http
+    if (image && image.length > 0) {
+      image.forEach(function (item, index, array) {
+        let result = item.split(":");
+        
+        if (!validator.equals(result[0], 'https')) {
+          return next(appError(400, '格式錯誤', '圖片格式不正確!'));
+        }
+      });
+    }
+
+    if (!content)
+      return next(appError(400, '格式錯誤', '欄位未填寫正確!'));
+
+    const ExistPost = await Post.findById(postId).exec();
+    if (!ExistPost)
+      return next(appError(400, '資料錯誤', '尚未發布貼文!'));
+    if (ExistPost.editor.toString() !== user._id.toString())
+      return next(appError(400, '資料錯誤', '您無權限編輯此貼文'));
+      
+    await Post.findByIdAndUpdate(postId, { editor: user, content, image });
+    const editPost = await Post.find({}).sort({_id:-1}).limit(1).select('-logicDeleteFlag');
+
+    res.status(201).json(getHttpResponse(editPost));
+  })
 }
 
 module.exports = posts;
