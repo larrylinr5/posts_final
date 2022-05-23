1a23347214773bb156b116c2e1b9afe710342a71
Hobby Lee <hobbylee@HobbydeMacBook-Pro.local>
2022-05-23 22:54:22 +0800

    fix: post.js 衝突

--- routes/posts.js
+++ routes/posts.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const PostController = require('../controller/postController');
-const LikesControllers = require('../controller/likeController')
+const LikesControllers = require('../controller/likeController');
+const FollowController = require('../controller/followController');
 const { isAuth } = require('../middleware/auth')
 
 // 張貼個人動態 - 新增貼文
 router.post('/', isAuth, (req, res, next) => {
-    PostController.postOnePost(req, res, next);
+  PostController.postOnePost(req, res, next);
 });
 
 // 取得個人按讚列表
 router.get('/likes', isAuth, (req, res, next) => {
-    LikesControllers.getUserLikeList(req, res, next)
+  LikesControllers.getUserLikeList(req, res, next)
 })
 
 // 新增一則貼文的讚
 router.put('/likes/:postId', isAuth, (req, res, next) => {
-    LikesControllers.addPostLike(req, res, next)
+  LikesControllers.addPostLike(req, res, next)
 })
 
 // 取消一則貼文的讚
 router.delete('/likes/:postId', isAuth, (req, res, next) => {
-    LikesControllers.delPostLike(req, res, next)
+  LikesControllers.delPostLike(req, res, next)
+)}
+
+/* 取得個人所有追蹤列表 */
+router.get('/follows', isAuth, (req, res, next) => {
+  FollowController.getFollowList(req, res, next)
 })
 
 module.exports = router;
