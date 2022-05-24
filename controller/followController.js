fd5c9ba215acdfa80b2c18db97307321c989273d
Hobby Lee <hobbylee@HobbydeMacBook-Pro.local>
2022-05-24 15:40:15 +0800

    revise: 取得追蹤清單- 加上當前頁碼、單頁幾筆驗證; 取得清單加上limit限制筆數

--- controller/followController.js
+++ controller/followController.js
@@ -53,11 +53,11 @@ const follows = {
   }),
   // 取得個人所有追蹤列表
   getFollowList: handleErrorAsync(async (req, res, next) => {
-    const { sort, q, currentPage, perPage } = req.query
+    let { sort, q, currentPage, perPage } = req.query
+
     // 時間排序
     const timeSort = sort === 'asc' ? 1 : -1
 
-    /* 頁碼處理 */
     // 取得總筆數
     const totalDatas = await Follow.aggregate([
       {
@@ -90,6 +90,7 @@ const follows = {
       }
     ])
 
+    // 若沒有資料，則回傳空陣列
     if (totalDatas.length === 0) {
       res.json({
         status: 'success',
@@ -107,21 +108,33 @@ const follows = {
       });
     }
 
+    /*
+      當前頁碼處理
+        若輸入參數不為整數，則回傳預設值 0
+    */
+    currentPage = validator.isInt(currentPage.toString())
+      ? Math.max(0, Number(currentPage - 1))
+      : 0
+
+    /* 單頁筆數處理
+        若輸入參數不為整數，且<0 ，則回傳預設值 10
+    */
+    perPage = validator.isInt(perPage.toString()) && Number(perPage) > 0
+      ? Number(perPage)
+      : 10
+
     // 算出總頁數
     const totalPages = Math.ceil(totalDatas[0].totalDatas / perPage)
 
-    // 若頁碼不為整數
-    if (!currentPage || !validator.isInt(currentPage.toString())) {
-      return next(appError('400', '資料內容有誤', `請輸入正確頁碼，共有${totalPages}頁`))
-    }
-
-    // 若總頁數 >0 且 page 大於最大頁數
-    if (totalPages > 0 && currentPage > totalPages) {
+    // 若總頁數 >0 且 當前頁碼 大於等於 最大頁數
+    if (totalPages > 0 && currentPage >= totalPages) {
       return next(appError('400', '資料內容有誤', `請輸入正確頁碼，共有${totalPages}頁`))
     }
 
     // 要跳過的筆數
-    const skip = (currentPage - 1) * perPage
+    const skip = currentPage * perPage
+
+    // 取得列表
     const list = await Follow.aggregate([
       {
         $lookup: {
@@ -155,6 +168,9 @@ const follows = {
         $sort: {
           createdAt: timeSort
         }
+      },
+      {
+        $limit: perPage
       }
     ])
 
@@ -164,11 +180,11 @@ const follows = {
       data: {
         page: {
           totalPages: totalPages, // 總頁數
-          currentPage: currentPage, // 當前頁數
+          currentPage: currentPage + 1, // 當前頁數
           perPage: perPage, // 一頁顯示資料筆數
           totalDatas: totalDatas[0].totalDatas, // 資料總筆數
-          has_pre: currentPage > 1, // 是否有前一頁
-          has_next: currentPage < totalPages // 是否有下一頁
+          has_pre: (currentPage + 1) > 1, // 是否有前一頁
+          has_next: (currentPage + 1) < totalPages // 是否有下一頁
         },
         list: list
       }
