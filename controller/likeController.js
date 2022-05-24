83ac239133a89d7412055f6913f862ec5b54717e
Adam Hsu <projectnba77@gmail.com>
2022-05-24 13:20:16 +0800

    fix: 取得個人按讚列表API，currentPage參數調整

--- controller/likeController.js
+++ controller/likeController.js
@@ -3,12 +3,12 @@ const getHttpResponse = require('../utils/successHandler');
 const Post = require('../models/postModel')
 
 const like = {
-    getUserLikeList: handleErrorAsync(async(req, res, next) => {
+    getUserLikeList: handleErrorAsync(async (req, res, next) => {
 
         const { user, query } = req
-        
+
         const keyword = query.q ? query.q : '' // 關鍵字
-        let currentPage = Number(query.currentPage - 1)// 當前頁數
+        let currentPage = Math.max(0, Number(query.currentPage - 1)) // 當前頁數
         let perPage = query.perPage ? Number(query.perPage) : 10 // 一頁顯示幾筆資料
 
         // 搜尋條件
@@ -23,7 +23,7 @@ const like = {
 
         // 倒序: desc，升序: asc
         const sort = query.sort === 'desc' ? -1 : query.sort === 'asc' ? 1 : 'asc'
-            // 用戶有按讚的所有貼文，隱藏comments欄位
+        // 用戶有按讚的所有貼文，隱藏comments欄位
         const userAllPost = await Post.find(filter, { 'comments': false }).populate({ path: 'editor', select: 'nickName avatar' }).skip(currentPage).limit(perPage).sort({ 'createdAt': sort })
 
         let totalDatas = userAllPost.length // 總資料筆數
@@ -43,7 +43,7 @@ const like = {
         res.status(201).json(getHttpResponse(resData));
     }),
 
-    addPostLike: handleErrorAsync(async(req, res, next) => {
+    addPostLike: handleErrorAsync(async (req, res, next) => {
         const { user, params: { postId } } = req
 
         await Post.findOneAndUpdate({ postId }, { $addToSet: { likes: user._id } })
@@ -51,7 +51,7 @@ const like = {
         res.status(201).json(getHttpResponse({ message: '加入按讚成功' }));
     }),
 
-    delPostLike: handleErrorAsync(async(req, res, next) => {
+    delPostLike: handleErrorAsync(async (req, res, next) => {
         const { user, params: { postId } } = req
 
         await Post.findOneAndUpdate({ postId }, { $pull: { likes: user._id } })
