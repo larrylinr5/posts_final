65592f425877f79f41f75dddc628a1321f05e14a
hiYifang <a6426320@gmail.com>
2022-06-01 00:49:21 +0800

    fix: 修正 postController.js

--- controller/postController.js
+++ controller/postController.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
-const { appError, handleErrorAsync } = require("../utils/errorHandler");
+const { appError } = require("../utils/errorHandler");
 const getHttpResponse = require("../utils/successHandler");
 const validator = require("validator");
 const Post = require("../models/postModel");
 const Comment = require("../models/commentModel");
 
-
 const posts = {
   // 取得全部貼文或個人全部貼文
-  getAllPosts: handleErrorAsync(async (req, res, next) => {
-    const { query, params: { userId } } = req;
+  getAllPosts: async (req, res) => {
+    const { 
+      query, 
+      params: { 
+        userId 
+      } 
+    } = req;
     const currentPage = query.currentPage ? Math.max(0, Number(query.currentPage - 1)) : 0;
     const perPage = query.perPage ? Number(query.perPage) : 10;
     const queryString = query.q !== undefined
@@ -73,7 +77,7 @@ const posts = {
     const totalPages = Math.ceil(total / perPage);
 
     const message = targetPosts.length === 0 ? "搜尋無資料" : "成功取得搜尋貼文";
-    const resData = {
+    const data = {
       list: targetPosts,
       page: {
         totalPages,
@@ -85,68 +89,82 @@ const posts = {
       }
     };
 
-    res.status(200).json(getHttpResponse({ data: resData, message }));
-  }),
-
+    res.status(200).json(getHttpResponse({ data, message }));
+  },
   // 新增貼文
-  postOnePost: handleErrorAsync(async (req, res, next) => {
-    const { user, body: { content, image } } = req;
+  postOnePost: async (req, res, next) => {
+    const { 
+      user, 
+      body: { 
+        content, 
+        image 
+      } 
+    } = req;
 
     // 判斷圖片開頭是否為 http
     if (image && image.length > 0) {
-      image.forEach(function (item, index, array) {
+      image.forEach(function (item) {
         let result = item.split(":");
         if (!validator.equals(result[0], "https")) {
-          return next(appError(400, "格式錯誤", "圖片格式不正確!"));
+          return next(appError(400, "40001", "圖片格式不正確!"));
         }
       });
     }
-
     if (!content)
-      return next(appError(400, "格式錯誤", "欄位未填寫正確!"));
+      return next(appError(400, "40001", "欄位未填寫正確!"));
 
     await Post.create({ editor: user, content, image });
     const newPost = await Post.find({}).sort({ _id: -1 }).limit(1).select("-logicDeleteFlag");
-
     res.status(201).json(getHttpResponse({ data: newPost }));
-  }),
+  },
   // 修改貼文
-  patchOnePost: handleErrorAsync(async (req, res, next) => {
-    const { user, body: { content, image }, params: { postId } } = req;
+  patchOnePost: async (req, res, next) => {
+    const { 
+      user, 
+      body: { 
+        content, 
+        image 
+      }, 
+      params: { 
+        postId 
+      } 
+    } = req;
 
     if (!(postId && mongoose.Types.ObjectId.isValid(postId)))
-      return next(appError(400, "資料錯誤", "請傳入特定貼文"));
-
+      return next(appError(400, "40002", "請傳入特定貼文"));
     // 判斷圖片開頭是否為 http
     if (image && image.length > 0) {
-      image.forEach(function (item, index, array) {
+      image.forEach(function (item) {
         let result = item.split(":");
         if (!validator.equals(result[0], "https")) {
-          return next(appError(400, "格式錯誤", "圖片格式不正確!"));
+          return next(appError(400, "40001", "圖片格式不正確!"));
         }
       });
     }
-
     if (!content)
-      return next(appError(400, "格式錯誤", "欄位未填寫正確!"));
+      return next(appError(400, "40001", "欄位未填寫正確!"));
 
     const ExistPost = await Post.findById(postId).exec();
     if (!ExistPost)
-      return next(appError(400, "資料錯誤", "尚未發布貼文!"));
+      return next(appError(400, "40010", "尚未發布貼文!"));
     if (ExistPost.editor.toString() !== user._id.toString())
-      return next(appError(400, "資料錯誤", "您無權限編輯此貼文"));
+      return next(appError(400, "40004", "您無權限編輯此貼文"));
 
     await Post.findByIdAndUpdate(postId, { content, image });
     const editPost = await Post.findOne({ _id: postId }).limit(1).select("-logicDeleteFlag");
     res.status(201).json(getHttpResponse({ data: editPost }));
-  }),
-
+  },
   // 刪除一筆貼文
-  deleteOnePost: handleErrorAsync(async (req, res, next) => {
-    const { user, params: { postId } } = req;
+  deleteOnePost: async (req, res, next) => {
+    const { 
+      user, 
+      params: { 
+        postId 
+      } 
+    } = req;
 
     if (!(postId && mongoose.Types.ObjectId.isValid(postId)))
-      return next(appError(400, "資料錯誤", "請傳入特定貼文"));
+      return next(appError(400, "40002", "請傳入特定貼文"));
 
     const filter = {
       "_id": postId,
@@ -154,17 +172,22 @@ const posts = {
     };
     const existPost = await Post.findOne(filter);
     if (!existPost)
-      return next(appError(400, "資料錯誤", "無此貼文!"));
+      return next(appError(400, "40010", "無此貼文!"));
 
     if (existPost.editor.toString() !== user._id.toString())
-      return next(appError(400, "資料錯誤", "您無權限編輯此貼文"));
+      return next(appError(400, "40004", "您無權限編輯此貼文"));
 
-    //執行刪除Post，把logicDeleteFlag設為true
-    await Post.findOneAndUpdate({ "_id": postId }, {
-      $set: { "logicDeleteFlag": true }
-    });
+    // 執行刪除 Post，把 logicDeleteFlag 設為 true
+    await Post.findOneAndUpdate(
+      { 
+        "_id": postId 
+      },
+      {
+        $set: { "logicDeleteFlag": true }
+      }
+    );
 
-    //執行刪除Comments，把logicDeleteFlag設為true
+    // 執行刪除 Comments，把 logicDeleteFlag 設為 true
     await Comment.updateMany(
       {
         _id: { $in: existPost.comments }
@@ -174,7 +197,7 @@ const posts = {
       }
     );
     res.status(201).json(getHttpResponse({ message: "刪除貼文成功!" }));
-  })
+  }
 };
 
 module.exports = posts;
