4c0c09163a9f01a3681d301522ae28ef02b3fe8c
hiYifang <a6426320@gmail.com>
2022-06-01 08:07:29 +0800

    fix: 修正 userController.js 寫作風格

--- controller/userController.js
+++ controller/userController.js
@@ -9,19 +9,21 @@ const users = {
   signUpCheck: async (req, res, next) => {
     const validatorResult = Validator.signUpCheck(req.body);
     if (!validatorResult.status) {
-      return next(appError("400", "40001", validatorResult.msg));
+      return next(appError(400, "40001", validatorResult.msg));
     }
     const { email } = req.body;
     const user = await User.find({ email });
     if (user.length > 0) {
-      return next(appError("400", "40002", "已註冊此用戶"));
+      return next(appError(400, "40002", "已註冊此用戶"));
     }
-    res.status(201).json(getHttpResponse({ message: "驗證成功" }));
+    res.status(201).json(getHttpResponse({
+      message: "驗證成功"
+    }));
   },
   signUp: async (req, res, next) => {
     const validatorResult = Validator.signUp(req.body);
     if (!validatorResult.status) {
-      return next(appError("400", "40001", validatorResult.msg));
+      return next(appError(400, "40001", validatorResult.msg));
     }
     password = await bcrypt.hash(req.body.password, 12);
     const { nickName, email } = req.body;
@@ -34,64 +36,74 @@ const users = {
       });
     } catch (error) {
       if (error.code === 11000) {
-        return next(appError("400", "40011", "已註冊此用戶"));
+        return next(appError(400, "40011", "已註冊此用戶"));
       }
-      return next(appError("400", "40005", "不明原因錯誤"));
+      return next(appError(400, "40005", "不明原因錯誤"));
     }
 
     const { _id } = newUser;
     const token = await generateJwtToken(_id);
     if (token.length === 0) {
-      return next(appError("400", "40003", "token 建立失敗"));
+      return next(appError(400, "40003", "token 建立失敗"));
     }
     const data = {
       token,
       "id": _id
     };
-    res.status(201).json(getHttpResponse({ data }));
+    res.status(201).json(getHttpResponse({
+      data
+    }));
   },
   signIn: async (req, res, next) => {
     const validatorResult = Validator.signIn(req.body);
     if (!validatorResult.status) {
-      return next(appError("400", "40001", validatorResult.msg));
+      return next(appError(400, "40001", validatorResult.msg));
     }
     const { email, password } = req.body;
     const user = await User.findOne({
       email
     }).select("+password");
     if (!user) {
-      return next(appError("400", "40010", "不存在該筆資料"));
+      return next(appError(400, "40010", "不存在該筆資料"));
     }
     const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
-      return next(appError("400", "40002", "您的密碼不正確"));
+      return next(appError(400, "40002", "您的密碼不正確"));
     }
     const { _id } = user;
     const token = await generateJwtToken(_id);
     if (token.length === 0) {
-      return next(appError("400", "40003", "token 建立失敗"));
+      return next(appError(400, "40003", "token 建立失敗"));
     }
     const data = {
       token,
       "id": _id
     };
-    res.status(201).json(getHttpResponse({ data }));
+    res.status(201).json(getHttpResponse({
+      data
+    }));
   },
   // 更新會員密碼
   updatePassword: async (req, res, next) => {
     const {
       user,
-      body: { 
-        password, 
-        confirm_password: confirmPassword, 
-        old_password: oldPassword 
+      body: {
+        password,
+        confirm_password: confirmPassword,
+        old_password: oldPassword
       },
     } = req;
-    const validatorResult = Validator.updatePw({ password, confirmPassword, oldPassword });
+    const validatorResult = Validator.updatePw({
+      password,
+      confirmPassword,
+      oldPassword
+    });
     if (!validatorResult.status) {
       return next(appError(400, "40001", validatorResult.msg, next));
     }
-    const users = await User.findOne({_id: user._id}).select("+password");
+    const users = await User.findOne({
+      _id: user._id
+    }).select("+password");
     const compare = await bcrypt.compare(oldPassword, users.password);
     if (!compare) {
       return next(appError(400, "40002", "您的舊密碼不正確!"));
@@ -99,33 +111,59 @@ const users = {
 
     users.password = null;
     const newPassword = await bcrypt.hash(req.body.password, 12);
-    await User.updateOne({ _id: user._id }, { password: newPassword });
-    res.status(201).json(getHttpResponse({ message: "更新密碼成功" }));
+    await User.updateOne(
+      {
+        _id: user._id
+      },
+      {
+        password: newPassword
+      });
+    res.status(201).json(getHttpResponse({
+      message: "更新密碼成功"
+    }));
   },
-  getMyProfile: async (req, res, next) => {
+  getMyProfile: async (req, res) => {
     const { user } = req;
     const profile = await User.findById(user._id).select("-logicDeleteFlag");
-    res.status(200).json(getHttpResponse({ data: profile }));
+    res.status(200).json(getHttpResponse({
+      data: profile
+    }));
   },
-  getOtherProfile: async (req, res, next) => {
+  getOtherProfile: async (req, res) => {
     const { userId } = req.params;
     const profile = await User.findById(userId).select("-logicDeleteFlag");
-    res.status(200).json(getHttpResponse({ data: profile }));
+    res.status(200).json(getHttpResponse({
+      data: profile
+    }));
   },
   updateProfile: async (req, res, next) => {
-    const { 
-      user, 
-      params: { userId }, 
-      body: { 
-        nickName, 
-        gender, 
-        avatar 
-      } 
+    const {
+      user,
+      params: { userId },
+      body: {
+        nickName,
+        gender,
+        avatar
+      }
     } = req;
-    if (String(user._id) !== String(userId)) return next(appError("400", "40004", "您無權限修改他人資料"));
-    if (!nickName || nickName.trim().length === 0) return next(appError("400", "40001", "請填寫暱稱"));
-    const profile = await User.findByIdAndUpdate(userId, { nickName, gender, avatar }, { new: true }).select("-logicDeleteFlag");
-    res.status(201).json(getHttpResponse({ data: profile }));
+    if (String(user._id) !== String(userId)) { 
+      return next(appError(400, "40004", "您無權限修改他人資料")) 
+    };
+    if (!nickName || nickName.trim().length === 0) { 
+      return next(appError(400, "40001", "請填寫暱稱")) 
+    };
+    const profile = await User.findByIdAndUpdate(userId,
+      {
+        nickName,
+        gender,
+        avatar
+      },
+      {
+        new: true
+      }).select("-logicDeleteFlag");
+    res.status(201).json(getHttpResponse({
+      data: profile
+    }));
   }
 };
 
