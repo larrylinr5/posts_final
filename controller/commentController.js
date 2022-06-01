362cb13960f0ae03c9ea7d7f8f5a74e260dfa6dc
hiYifang <a6426320@gmail.com>
2022-06-01 08:49:58 +0800

    fix: 修正 commentController.js 寫作風格

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { appError, handleErrorAsync } = require("../utils/errorHandler");
+const { appError } = require("../utils/errorHandler");
 const getHttpResponse = require("../utils/successHandler");
 const Post = require("../models/postModel");
 const Comment = require("../models/commentModel");
@@ -17,21 +17,32 @@ const comments = {
       } 
     } = req;
 
-    if (!(postId && mongoose.Types.ObjectId.isValid(postId)))
+    if (!(postId && mongoose.Types.ObjectId.isValid(postId))){
       return next(appError(400, "40002", "請傳入特定貼文"));
-    if (!comment)
+    }
+    if (!comment){
       return next(appError(400, "40001", "請填寫留言內容!"));
+    }
 
     const ExistPost = await Post.findById(postId).exec();
-    if (!ExistPost)
+    if (!ExistPost){
       return next(appError(400, "40010", "尚未發布貼文!"));
+    }
 
     const newComment = await Comment.create({ editor: user, comment });
     await Post.updateOne(
-      { _id: postId }, 
-      { comments: [...ExistPost.comments, newComment._id] }
+      { 
+        _id: postId
+      }, 
+      { 
+        comments: [...ExistPost.comments, newComment._id]
+      }
     );
-    res.status(201).json(getHttpResponse({ data: { comment: newComment } }));
+    res.status(201).json(getHttpResponse({ 
+      data: { 
+        comment: newComment 
+      } 
+    }));
   },
   // 修改一則貼文的留言
   patchPostComment: async (req, res, next) => {
@@ -45,21 +56,30 @@ const comments = {
       } 
     } = req;
 
-    if (!(commentId && mongoose.Types.ObjectId.isValid(commentId)))
+    if (!(commentId && mongoose.Types.ObjectId.isValid(commentId))){
       return next(appError(400, "40002", "請傳入特定留言"));
-    if (!comment)
+    }
+    if (!comment){
       return next(appError(400, "40001", "請填寫留言內容!"));
+    }
 
     const ExistPost = await Post.findById(postId).exec();
-    if (!ExistPost)
+    if (!ExistPost){
       return next(appError(400, "40010", "尚未發布貼文!"));
+    }
+
     const ExistComment = await Comment.findById(commentId).exec();
-    if (!ExistComment)
+    if (!ExistComment){
       return next(appError(400, "40010", "尚未發布留言!"));
+    }
 
     const editComment = await Comment.findByIdAndUpdate(commentId, { comment });
     const patchComment = await Comment.findById(editComment._id).populate({ path: "editor", select: "nickName avatar" });
-    res.status(201).json(getHttpResponse({ data: { comment: patchComment } }));
+    res.status(201).json(getHttpResponse({ 
+      data: { 
+        comment: patchComment
+      } 
+    }));
   },
   // 刪除一則貼文的留言
   deletePostComment: async (req, res, next) => {
@@ -70,22 +90,32 @@ const comments = {
       } 
     } = req;
 
-    if (!(commentId && mongoose.Types.ObjectId.isValid(commentId)))
+    if (!(commentId && mongoose.Types.ObjectId.isValid(commentId))){
       return next(appError(400, "40002", "請傳入特定留言"));
+    }
 
     const ExistPost = await Post.findById(postId).exec();
-    if (!ExistPost)
+    if (!ExistPost){
       return next(appError(400, "40010", "尚未發布貼文!"));
+    }
+
     const ExistComment = await Comment.findById(commentId).exec();
-    if (!ExistComment)
+    if (!ExistComment){
       return next(appError(400, "40010", "尚未發布留言!"));
+    }
 
     // 執行刪除，其實是把 Comment 的 logicDeleteFlag 設為 true
     await Comment.findOneAndUpdate(
-      { "_id": commentId }, 
-      { $set: { "logicDeleteFlag": true } }
+      { 
+        "_id": commentId
+      }, 
+      { 
+        $set: { "logicDeleteFlag": true } 
+      }
     );
-    res.status(201).json(getHttpResponse({ message: "刪除留言成功！" }));
+    res.status(201).json(getHttpResponse({ 
+      message: "刪除留言成功！"
+    }));
   }
 };
 
