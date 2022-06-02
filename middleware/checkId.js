63d76c48765f8d8a9f290d3600c26b76e5ff80c1
hiYifang <a6426320@gmail.com>
2022-06-03 01:36:17 +0800

    fix: 修改 checkId.js

--- middleware/checkId.js
+++ middleware/checkId.js
@@ -6,7 +6,7 @@ const checkUserId = async(req, res, next) => {
   const { userId } = req.params;
   const isValidId = mongoose.Types.ObjectId.isValid(userId);
   if (!isValidId) {
-    return next(appError(400, "40002", "使用者 ID 格式錯誤, 請重新確認"));
+    return next(appError(400, "40001", "使用者 ID 格式錯誤, 請重新確認"));
   }
   const user = await User.findById(userId);
   if (!user) {
