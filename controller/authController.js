a83f173a6d7c84f66b4ae0d0e47979835a67b16e
hung <ff789456741@gmail.com>
2023-04-10 23:07:40 +0800

    feature: thirePart

--- controller/authController.js
+++ controller/authController.js
@@ -12,7 +12,32 @@ const authController = {
     execCallback: handleErrorAsync(async (req, res, next) => {
       passport.authenticate("google")(req, res, next);
     })
+  },
+  discord: {
+    auth: handleErrorAsync(async (req, res, next) => {
+      passport.authenticate("discord")(req, res, next);
+    }),
+    execCallback: handleErrorAsync(async (req, res, next) => {
+      passport.authenticate("discord")(req, res, next);
+    })
+  },
+  facebook: {
+    auth: handleErrorAsync(async (req, res, next) => {
+      passport.authenticate("facebook", { scope: ["email", "public_profile"] })(req, res, next);
+    }),
+    execCallback: handleErrorAsync(async (req, res, next) => {
+      passport.authenticate("facebook")(req, res, next);
+    })
+  },
+  line: {
+    auth: handleErrorAsync(async (req, res, next) => {
+      passport.authenticate("line")(req, res, next);
+    }),
+    execCallback: handleErrorAsync(async (req, res, next) => {
+      passport.authenticate("line")(req, res, next);
+    })
   }
+  
 };
 
 module.exports = authController;
