d63af99223bfcceef35dde4d974eb50862cefff8
charlie50503 <cate50503@gmail.com>
2023-06-19 22:49:53 +0800

    feat: [即時聊天] 添加 mongodb 事務功能
    讓commit 可以建立在同一個session上
    如果有一個db 處理不成功
    整段session都不會更新上去

--- socket/controller/chatMessageController.js
+++ socket/controller/chatMessageController.js
@@ -1,5 +1,9 @@
 const ChatMessages = require("../../models/chatMessagesModel");
+const ConversationUnread = require("../../models/conversationUnreadModel");
+// @ts-ignore
 const User = require("../../models/userModel");
+const { transactionHandler } = require("../../utils/commitHandler");
+// @ts-ignore
 const { decodedUserId } = require("../middleware/auth");
 const SocketResponse = require("../response/response");
 const chatMessages = {
@@ -7,18 +11,19 @@ const chatMessages = {
     console.log("chat", data);
     // 更新資料庫跟顯示的時間差會不會有bug
     const chatMessage = await chatMessages.setChatMessages(data);
+    console.log("chatMessage", chatMessage);
     // 操作成功，向客户端发送成功的消息
     const response = new SocketResponse({
       statusCode: "success",
       message: "",
       data: chatMessage,
-      error: null
+      error: null,
     });
     io.sockets.in(data.roomId).emit("chatResponse", response);
   },
-  getMessagesHandler: async (socket, {roomId}) => {
+  getMessagesHandler: async (socket, { roomId }) => {
     console.log("getMessages", roomId);
-    if(!roomId){
+    if (!roomId) {
       throw Error("找不到 roomId");
     }
     const messages = await ChatMessages.find({
@@ -34,27 +39,61 @@ const chatMessages = {
       statusCode: "success",
       message: "",
       data: messages,
-      error: null
+      error: null,
     });
     socket.emit("getMessagesResponse", response);
   },
   async setChatMessages({ roomId, userId, text, image = "" }) {
-    const updatedMessage = await ChatMessages.create({
-      conversation: roomId,
-      sender: userId,
-      text: text,
-      image: image,
-    });
+    return await transactionHandler(async (session) => {
+      const opts = { session, upsert: true, returnOriginal: false };
 
-    const newMessage = await ChatMessages.findOne({
-      _id: updatedMessage._id,
-      logicDeleteFlag: false
-    }).populate({
-      path: "sender",
-      select: "nickName avatar",
+      const createdMessages = await ChatMessages.create(
+        [
+          {
+            conversation: roomId,
+            sender: userId,
+            text: text,
+            image: image,
+          },
+        ],
+        opts
+      );
+
+      // Update all users (except the sender) in the same conversation and increase their unreadCount
+      await ConversationUnread.updateMany(
+        {
+          conversation: roomId,
+          user: { $ne: userId }, // Exclude the sender
+          logicDeleteFlag: false,
+        },
+        {
+          $inc: { unreadCount: 1 }, // Use the $inc operator to increase the value of unreadCount
+        },
+        opts
+      );
+
+      const createdMessage = createdMessages[0];
+
+      // 這裡不再需要 `findOne` 來尋找剛創建的訊息
+      const newMessage = await ChatMessages.findOne(
+        {
+          _id: createdMessage._id,
+          logicDeleteFlag: false,
+        },
+        null,
+        { session }  // 加入這行
+      ).populate({
+        path: "sender",
+        select: "nickName avatar",
+      });
+      
+      // 檢查 newMessage 是否為 null 或 undefined
+      if (!newMessage) {
+        throw new Error(`Could not find message with ID ${createdMessage._id}`);
+      }
+      
+      return newMessage;
     });
-    
-    return newMessage;
   },
 };
 
---
+++ utils/commitHandler.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+// 事務處理器
+const transactionHandler = async (action) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    // 執行傳入的操作
+    const result = await action(session);
+    console.log('操作成功，結果為：', result);
+    // 如果操作成功，則提交事務
+    await session.commitTransaction();
+
+    return result;
+  } catch (error) {
+    // 如果操作失敗，則中止事務
+    await session.abortTransaction();
+
+    throw error;
+  } finally {
+    // 結束session
+    session.endSession();
+  }
+};
+
+
+module.exports = {
+  transactionHandler
+};
