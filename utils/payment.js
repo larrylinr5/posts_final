6f0853c20f18281c4827c9b187a33a514c52ee5c
charlie50503 <cate50503@gmail.com>
2023-08-21 23:55:41 +0800

    fix: [藍新金流]→修正無法正確訪問到藍新金流頁面問題
    因 doppler 的環境變量都是大寫
    而本地設定的藍新金流變量都用大小寫
    導致正式環境抓不到
    
    所以全部統一用大寫

--- utils/payment.js
+++ utils/payment.js
@@ -1,10 +1,10 @@
 var crypto = require("crypto");
 // 字串組合
 function genDataChain(order) {
-  const { MerchantID, RespondType, Version } = process.env;
-  return `MerchantID=${MerchantID}&RespondType=${RespondType}&TimeStamp=${
+  const { MERCHANTID, RESPONDTYPE, VERSION } = process.env;
+  return `MerchantID=${MERCHANTID}&RespondType=${RESPONDTYPE}&TimeStamp=${
     order.TimeStamp
-  }&Version=${Version}&MerchantOrderNo=${order.MerchantOrderNo}&Amt=${
+  }&Version=${VERSION}&MerchantOrderNo=${order.MerchantOrderNo}&Amt=${
     order.Amt
   }&ItemDesc=${encodeURIComponent(order.ItemDesc)}&Email=${encodeURIComponent(
     order.Email,
@@ -13,25 +13,25 @@ function genDataChain(order) {
 // 對應文件 P16：使用 aes 加密
 // $edata1=bin2hex(openssl_encrypt($data1, "AES-256-CBC", $key, OPENSSL_RAW_DATA, $iv));
 function create_mpg_aes_encrypt(TradeInfo) {
-  const { HashKey, HashIV } = process.env;
-  const encrypt = crypto.createCipheriv("aes256", HashKey, HashIV);
+  const { HASHKEY, HASHIV } = process.env;
+  const encrypt = crypto.createCipheriv("aes256", HASHKEY, HASHIV);
   const enc = encrypt.update(genDataChain(TradeInfo), "utf8", "hex");
   return enc + encrypt.final("hex");
 }
 
 // 對應文件 P17：使用 sha256 加密
-// $hashs="HashKey=".$key."&".$edata1."&HashIV=".$iv;
+// $hashs="HashKey=".$key."&".$edata1."&HASHIV=".$iv;
 function create_mpg_sha_encrypt(aesEncrypt) {
-  const { HashKey, HashIV } = process.env;
+  const { HASHKEY, HASHIV } = process.env;
   const sha = crypto.createHash("sha256");
-  const plainText = `HashKey=${HashKey}&${aesEncrypt}&HashIV=${HashIV}`;
+  const plainText = `HashKey=${HASHKEY}&${aesEncrypt}&HashIV=${HASHIV}`;
   return sha.update(plainText).digest("hex").toUpperCase();
 }
 
 // 將 aes 解密
 function create_mpg_aes_decrypt(TradeInfo) {
-  const { HashKey, HashIV } = process.env;
-  const decrypt = crypto.createDecipheriv("aes256", HashKey, HashIV);
+  const { HASHKEY, HASHIV } = process.env;
+  const decrypt = crypto.createDecipheriv("aes256", HASHKEY, HASHIV);
   decrypt.setAutoPadding(false);
   const text = decrypt.update(TradeInfo, "hex", "utf8");
   const plainText = text + decrypt.final("utf8");
